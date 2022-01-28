cfd5b7883358f1a4d863eafa44aa8b106455794f
Mike <mike@mikeowino.com>
2022-01-28 21:50:35 +0300

    Update mike.py

--- mike.py
+++ mike.py
@@ -1,10 +1,32 @@
+from datetime import datetime
+import pytz
 
-# using time module
-import time
+sasa = datetime.now()
+# sleep(1)
+# current_time = sasa.strftime("%H:%M:%S")
+# print("The time is", current_time)
 
-ts = time.time()
-print(ts)
+# sleep(1)
+# t = time.localtime()
+# current_time = time.strftime("%H:%M:%S", t)
+# print("The time is", current_time)
+
+# sleep(1)
+# tz_NY = pytz.timezone('America/New_York') 
+# datetime_NY = datetime.now(tz_NY)
+# print("NY time:", datetime_NY.strftime("%H:%M:%S"))
+
+KenyaTime = pytz.timezone('Africa/Nairobi')
+datetime_Kenya = datetime.now(KenyaTime)
+
+dafa = datetime_Kenya.strftime("%H:%M:%S")
+
+print("Kenyan time:", datetime_Kenya.strftime("%H:%M:%S"))
+
+
+
 
 file = open("mike.txt", 'w')
-file.write(str(ts))
+file.write("Kenyan Time is: ")
+file.write(str(dafa))
 file.close()

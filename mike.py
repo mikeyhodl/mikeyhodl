123bd44b4755fbc14a166e9d67dd9e277e2a7000
mikey <mike@mikeowino.com>
2022-05-01 21:15:01 +0300

    Update mike.py

--- mike.py
+++ mike.py
@@ -4,7 +4,7 @@ localtime = time.asctime( time.localtime(time.time()) )
 
 execTime = localtime
 
-file = open("readme.txt", 'w')
+file = open("mike.txt", 'w')
 file.write("Time of run : ")
 file.write(str(execTime))
 file.close()
